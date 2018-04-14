27380072acb2920a1f89261efbe697227c6224c0
erfanio <dev@erfan.io>
2018-04-14 19:53:04 +1000

    more packages install packages in arch setup

--- arch_setup.sh
+++ arch_setup.sh
@@ -17,6 +17,12 @@ ln -v -s ~/dotfiles/.xinitrc ~/.xinitrc
 ln -v -s ~/dotfiles/.Xresources ~/.Xresources
 
 echo "Creating a new SSH key"
-ssh-keygen -t rsa -b 4096 -o -a 100
+ssh-keygen -t rsa -b 4096 -a 100
+
+echo "Installing packages"
+yaourt -Sy $(cat dotfiles/packages) --noconfirm
+
+echo "Setting default apps"
+xdg-mime default Thunar.desktop inode/directory
 
 echo "You need to download something to .fehbg.jpg"
