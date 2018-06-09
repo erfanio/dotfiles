1518d789539ae05a695dbaaa2fe376ac81240d25
erfanio <dev@erfan.io>
2018-06-10 00:26:38 +1000

    update with master's changes

--- arch_setup.sh
+++ arch_setup.sh
@@ -18,4 +18,10 @@ ln -v -s ~/dotfiles/.xinitrc ~/.xinitrc
 echo "Creating a new SSH key"
 ssh-keygen -t rsa -b 4096 -a 100
 
+echo "Installing packages"
+yaourt -Sy $(cat dotfiles/packages) --noconfirm
+
+echo "Setting default apps"
+xdg-mime default Thunar.desktop inode/directory
+
 echo "You need to download something to .fehbg.jpg"
--- i3/dim-screen.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-trap "exit 0" INT TERM
-# figure out something to do here
-sleep 2147483647 &
-wait
